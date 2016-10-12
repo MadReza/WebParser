681db307edf84cee944d1e09b8c14a02c345d2ba
Ahmad Madabadi <a_madab@grace.encs.concordia.ca>
2016-10-12 13:03:15 -0400

    Added the stemming for historical purpose. Removing it for dictionnary soon

--- main.py
+++ main.py
@@ -13,27 +13,47 @@ fileNames = getFilePathsMatching("reut/*.sgm")
 import sys
 sys.path.append('beautifulsoup4-4.5.1')
 from bs4 import BeautifulSoup
-
+from stemming.porter2 import stem
 ##Empty Dict to store
 
 d = {}
-
+title = {}
+body = {}
 for file in fileNames:
 	f = open(file, "r")
         xmlTextList = BeautifulSoup(f, "html.parser").findAll("reuters")
         for xmlText in xmlTextList:
                 key = xmlText.get('newid')
-                d[key] = {}                
+                d[key] = {}
                 if xmlText.title:
+#                        print key + ": " + xmlText.title.text
                         d[key]['title'] = removeStopWords(xmlText.title.text)
+#                        print d[key]['title']
+#                        print d[key]['title'].split()
+                        for word in d[key]['title'].split():
+                                stemmed = stem(word)
+                                title[stemmed] = {}
+								if key in title[stemmed]:	#more efficient.
+                                #if title[stemmed].has_key(key):
+                                        title[stemmed][key] = title[stemmed][key] + 1
+                                else:
+                                        title[stemmed][key] = 1
                 if xmlText.date:
                         d[key]['date'] = xmlText.date.text.lower()
                 if xmlText.body:
                         d[key]['body'] = removeStopWords(xmlText.body.text)
+                        for word in d[key]['body'].split():
+                                stemmed = stem(word)
+                                body[stemmed] = {}
+                                if body[stemmed].has_key(key):
+                                        body[stemmed][key] = body[stemmed][key] + 1
+                                else:
+                                        body[stemmed][key] = 1
 
 
-for key, value in d.items():
+for key, value in title.items():
         for k, v in value.items():
-                if k == 'title':
-                        print key + ": " + v
+                print key + ": " + k + "@"  
+
+
                         
