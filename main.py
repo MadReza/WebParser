a0cc2f2ec19c47d8df6270c9da357dfc1ffcf00f
Ahmad Madabadi <rezmaza@gmail.com>
2016-10-14 23:24:00 -0400

    Optimized and tested some timing

--- main.py
+++ main.py
@@ -3,31 +3,29 @@ print "Start of Script"
 from utilities import getFilePathsMatching
 from utilities import removeStopWords
 
-fileNames = ""
-
 ##Retrieve the file Names and location from current location
 fileNames = getFilePathsMatching("reut/*.sgm")
 
 ##---------------------------
-
+import time
 import sys
 sys.path.append('beautifulsoup4-4.5.1')
 from bs4 import BeautifulSoup
 from stemming.porter2 import stem
-##Empty Dict to store
 
-d = {}
+##Empty Dict to store
 title = {}
+date = {}
 body = {}
+t = time.clock()
+
 for file in fileNames:
 	f = open(file, "r")
         xmlTextList = BeautifulSoup(f, "html.parser").findAll("reuters")
         for xmlText in xmlTextList:
                 key = xmlText.get('newid')
-                d[key] = {}
                 if xmlText.title:
-                        d[key]['title'] = removeStopWords(xmlText.title.text)
-                        for word in d[key]['title'].split():
+                        for word in removeStopWords(xmlText.title.text):
                                 stemmed = stem(word)
                                 if stemmed in title:
                                         if key in title[stemmed]:
@@ -38,11 +36,17 @@ for file in fileNames:
                                         title[stemmed] = {}
                                         title[stemmed][key] = 1
                 if xmlText.date:
-                        d[key]['date'] = xmlText.date.text.lower()
-                        ###TODO: There is some junk in few dates need cleaning.
+                        d = xmlText.date.text.lower()
+                        if d in date:
+                                if key in date[d]:
+                                        date[d][key] += 1
+                                else:
+                                        date[d][key] = 1
+                        else:
+                                date[d] = {}
+                                date[d][key] = 1
                 if xmlText.body:
-                        d[key]['body'] = removeStopWords(xmlText.body.text)
-                        for word in d[key]['body'].split():
+                        for word in removeStopWords(xmlText.body.text):
                                 stemmed = stem(word)
                                 if stemmed in body:
                                         if key in body[stemmed]:
@@ -52,21 +56,22 @@ for file in fileNames:
                                 else:
                                         body[stemmed] = {}
                                         body[stemmed][key] = 1
-        break
+        #break
 
+print `time.clock() - t`
 
 import json
 with open('all_title.json', 'w') as file_name:
         json.dump(title, file_name, sort_keys=True, indent=4)
 with open('all_body.json', 'w') as file_name:
         json.dump(body, file_name, sort_keys=True, indent=4)
-with open('test.json', 'w') as file_name:
-        json.dump(body, file_name, sort_keys=True, indent=4)
+with open('all_title.json', 'w') as file_name:
+        json.dump(title, file_name, sort_keys=True, indent=4)
 
-
+"""
 for key, value in title.items():
         for k, v in value.items():
-                print key + ": " + k + " @ " + v  
-
+                print `key` + ": " + `k` + " @ " + `v`  
+"""
 
                         
--- utilities.py
+++ utilities.py
@@ -1,8 +1,8 @@
-
 ###~~File Retriver~~###
 from glob import glob
 def getFilePathsMatching(path):
     return glob(path);
+
 
 ###~~Cleaning~~###
 import string
@@ -10,15 +10,7 @@ from nltk.corpus import stopwords
 from nltk import word_tokenize
 
 cached_junk = list(string.punctuation) + stopwords.words("english")
-#cachedStopWords = stopwords.words("english")
-#cachedPunctuation = list(string.punctuation)
 
-def removeStopWords(text):
-    return [i for i in word_tokenize(text.lower()) if i not in cached_junk]
+def removeStopWords(text):    
+    return (i for i in word_tokenize(text.lower()) if i not in cached_junk)
 
-
-    #str = str.lower()
-    #str = ''.join((char for char in word + " " if char not in cachedPunctuation) for word in str.split() if word not in cachedStopWords)
-    #str = ''.join(char for char in str if char not in cachedPunctuation)
-    #str = ''.join([word + " " for word in str.split() if word not in cachedStopWords])
-    return str
